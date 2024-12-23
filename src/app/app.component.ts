8ec220569e85cde9855e295c52c85fa5067724f2
CHINTAN_SHAH\chint <chintanshah97120@gmail.com>
2024-12-24 00:29:06 +0530

    New Update Create a responsive layout with a Next button to go to a new page, a Save button for form submission, and a progress line that dynamically adjusts on screen size.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 // import { RouterOutlet } from '@angular/router';
 
@@ -11,20 +11,26 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'], // Corrected from 'styleUrl' to 'styleUrls'
 })
-export class AppComponent {
-  stepsList: any[] = [
+export class AppComponent implements OnInit{
+  designationList: any[]= [];
+  roleList: any[]= []
+    stepsList: any[] = [
     { stepName: 'Basic Details', isComplete: false },
     { stepName: 'Skills', isComplete: false },
     { stepName: 'Experiences', isComplete: false },
   ];
   activeStep: any = this.stepsList[0];
 
+  // Create a variable of line-progress
+
+  stepperComplitionValue: number = 8;
+
   employeeObj: any={
     "roleId":0,
     "empId":0,
     "empName":"",
     "empEmailId":"",
-    "empDestiganationId":0,
+    "empDesignationId":0,
     "empContactNo": "",
     "empAltContactNo":"",
     "empPersonalEmailId":"",
@@ -59,9 +65,27 @@ export class AppComponent {
   constructor(private http:HttpClient){
 
   }
+  ngOnInit(): void {
+    this.loaDesignations();
+    this.loadRoles();
+  }
+
   setActiveStep(activeStep:any) {
     this.activeStep = activeStep;
 }
+gotoStep2(){
+  const currentStep = this.stepsList.find(m=>m.stepName == this.activeStep.stepName);
+  currentStep.isComplete = true;
+  this.activeStep = this.stepsList[1];
+  this.stepperComplitionValue = 50;
+}
+gotoStep3(){
+  const currentStep = this.stepsList.find(m=>m.stepName == this.activeStep.stepName);
+  currentStep.isComplete = true;
+  this.activeStep = this.stepsList[2];
+  this.stepperComplitionValue = 100;
+}
+
     addSkills(){
       const skillObj ={
         "empSkillId": 0,
@@ -84,8 +108,18 @@ export class AppComponent {
       }
       this.employeeObj.ermEmpExperiences.unshift(expObj)
     }
+    loaDesignations(){
+      this.http.get("https://freeapi.gerasim.in/api/EmployeeApp/GetAllDesignation").subscribe((res:any)=>{
+        this.designationList = res.data;
+      })
+    }
+    loadRoles(){
+      this.http.get("https://freeapi.gerasim.in/api/EmployeeApp/GetAllRoles").subscribe((res:any)=>{
+        this.roleList = res.data;
+      })
+    }
     saveEmployee(){
-      this.http.post("https://freeapi.gerasim.in/api/EmployeeApp/CreateNewEmployee",this.employeeObj).subscribe((res:any)=>{
+      this.http.post("https://faux-api.com/api/v1/createnewemployee_30968866493735203",this.employeeObj).subscribe((res:any)=>{
         debugger;
         if(res.result){
           alert('Employee Create Succesfully')
